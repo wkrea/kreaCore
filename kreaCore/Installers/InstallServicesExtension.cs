8fff06cd4b9496aeab93388475b9142cdc043d4f
wkrea <william.trigos@gmail.com>
2019-12-05 18:28:16 -0500

    Agregando algunos comentarios a la extensión de
    servicios, para probar la construcción del nuget desde DevOpsAzure
    
    https://dev.azure.com/javiertrigos88/kreaCore/_packaging?_a=feed&feed=kreaCore

--- kreaCore/Installers/InstallServicesExtension.cs
+++ kreaCore/Installers/InstallServicesExtension.cs
@@ -7,6 +7,14 @@ namespace kreaCore.Installers
 {
     public static class InstallServicesExtension
     {
+        /// <summary>
+        /// Metodo que permite la carga dinamica de los servicios desde 
+        /// Configure services en el archivo StartUp.cs de un proyecto
+        /// web NetCore
+        /// </summary>
+        /// <typeparam name="MyStartup">Provide your Startup class as Generic parameter</typeparam>
+        /// <param name="services"> Provide access to your services collection from Startup.cs </param>
+        /// <param name="configuration">Provide access to your IConfiguration intance from Startup.cs</param>
         public static void InstallServices<MyStartup>(
             this IServiceCollection services, 
             IConfiguration configuration) where MyStartup : class
